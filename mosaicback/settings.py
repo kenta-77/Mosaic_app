4165dc5acf9be4778c29f68e259c30d759a19ae9
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2023-03-03 07:07:12 +0900

    cors

--- mosaicback/settings.py
+++ mosaicback/settings.py
@@ -144,7 +144,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = [
     'https://face-mosaic.com',
-    'https://face-mosaic.com/main',
     'https://kenta-77.github.io',
     'http://127.0.0.1:3000',
     'http://localhost:3000',
