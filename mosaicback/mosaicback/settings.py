93a27b707562cf4551a69799bae7dd9afcaedd59
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2023-03-03 03:18:39 +0900

    setting修正

--- mosaicback/mosaicback/settings.py
+++ mosaicback/mosaicback/settings.py
@@ -15,7 +15,7 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent + '/mosaicback'
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
