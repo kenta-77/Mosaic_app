cf26788eaff225e881c49f2a699c929ef4d43315
chinatsu <101545251+chinatsu118@users.noreply.github.com>
2023-03-02 23:41:30 +0900

    Change top design

--- pages/top.tsx
+++ pages/top.tsx
@@ -4,26 +4,36 @@ import Head from "next/head";
 import { Box,Flex,Heading, Button, Text, Center} from "@chakra-ui/react";
 import Image from 'next/image';
 import Layout from '../components/Layout'
+import { filledInputClasses } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
     <>
     <Layout> </Layout>
       <Center h='200px' mt='100px'>
+        <Image
+          src='/back_image.jpg'
+          alt='test_image'
+          width="1300"
+          height="300"
+        />
+      </Center>
+      <Center>
         <Heading
           bgGradient='linear(to-l, #7928CA, #FF0080)'
           bgClip='text'
           fontSize='6xl'
           fontWeight='extrabold'
+          className="h2"
         >Welcome to FaMo</Heading>
-      </Center>
+        </Center>
       <Center>
         <Text fontSize='3xl'>顔にモザイクをかけるアプリです</Text>
       </Center>
       <Box mt='50px' h='200px'>
         <Center>
-          <Link href='./'>
-            <Button>はじめる</Button>
+          <Link href='/'>
+            <Button>START</Button>
           </Link>
         </Center>
       </Box>
