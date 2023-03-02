faaa35a09caeba892d489bcedb913bcc6c67107a
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2023-03-03 05:36:38 +0900

    db

--- mosaicback/settings.py
+++ mosaicback/settings.py
@@ -94,8 +94,8 @@ WSGI_APPLICATION = "mosaicback.wsgi.application"
 # }
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'DetaBase',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
     'USER': 'mosaicer',
     'PASSWORD': 'MoPos-88er',
     'HOST': 'host',
